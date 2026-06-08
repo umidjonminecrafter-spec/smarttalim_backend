b1a03d6c3d2f590266d464482cee2d2ae58c6923
beautiful <umidjonminecrafter@gmail.com>
2026-06-08 14:22:30 +0500

    Men zo'rman

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = [
     "smarttalim-backend.onrender.com",
     "localhost",
